eea03a337784d8f23124704fcadf56da34585a4d
Sakar <singhalsakar@gmail.com>
2019-10-29 04:51:27 +0530

    animation and visual elements added

--- app/src/main/java/com/example/myapplication/Loginpage.java
+++ app/src/main/java/com/example/myapplication/Loginpage.java
@@ -1,13 +1,21 @@
 package com.example.myapplication;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Explode;
+import android.view.Window;
 
 public class Loginpage extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+        getWindow().setEnterTransition(new Explode());
         setContentView(R.layout.activity_loginpage);
+
     }
 }
--- app/src/main/java/com/example/myapplication/SplashScreen.java
+++ app/src/main/java/com/example/myapplication/SplashScreen.java
@@ -1,23 +1,38 @@
 package com.example.myapplication;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Handler;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Explode;
+import android.transition.Transition;
+import android.util.Pair;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
 Handler handler;
+ImageView img;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         handler=new Handler();
+        img=findViewById(R.id.logo);
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(getApplicationContext(),Loginpage.class));
+
+                ActivityOptionsCompat activityOptionsCompat = (ActivityOptionsCompat) ActivityOptionsCompat.makeSceneTransitionAnimation(SplashScreen.this,(View)img,"zoomout");
+                startActivity(new Intent(getApplicationContext(),Loginpage.class),activityOptionsCompat.toBundle());
             }
-        },10000);
+        },5000);
 
     }
 }
