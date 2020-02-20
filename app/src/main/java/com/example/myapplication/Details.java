097bcb142110f84af2c3dfef3874460c6ecbbb66
Sakar <singhalsakar@gmail.com>
2020-02-20 17:47:51 +0530

    a lot shit dude3

--- app/src/main/java/com/example/myapplication/Details.java
+++ app/src/main/java/com/example/myapplication/Details.java
@@ -1,7 +1,11 @@
 package com.example.myapplication;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Details extends AppCompatActivity {
@@ -16,7 +20,13 @@ public class Details extends AppCompatActivity {
         TextView bookdescription=findViewById(R.id.bookdescription);
         TextView bookprice=findViewById(R.id.bookprice);
         TextView bookgenre=findViewById(R.id.bookgenre);
+        ImageView frontcover=findViewById(R.id.frontcover);
         bookname.setText(MyData.name.get(p));
-        bookauthor.setText(MyData.price.get(p));
+        bookauthor.setText(MyData.authorname.get(p));
+        bookprice.setText(MyData.price.get(p));
+        bookdescription.setText(MyData.description.get(p));
+        byte[] decode= Base64.decode(MyData.image.get(p),Base64.DEFAULT);
+        Bitmap decoded= BitmapFactory.decodeByteArray(decode,0,decode.length);
+        frontcover.setImageBitmap(decoded);
     }
 }
